db681928c43e6f7ce62b25558043c91217f44128
Billy <billyvietlee@gmail.com>
2023-03-20 22:56:49 -0700

    added connection to database

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const path = require("path");
 
-require("dotenv").config();
 
-const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-} = process.env;
+
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
